e9ca9cf75d015e661d9277606a6ff5fa05935237
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-01-21 16:51:34 -0300

    feat: beginning project

---
+++ styles/style.ts
@@ -0,0 +1,88 @@
+import styled from "styled-components";
+import { Button, Checkbox, TextareaAutosize } from "@material-ui/core";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background-color: #fafafa;
+  gap: 25px;
+`;
+
+export const CustomTextArea = styled(TextareaAutosize)`
+  width: 150px;
+  height: 50px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  resize: none;
+`;
+
+export const CustomButton = styled(Button)`
+  width: 150px;
+  height: 37px;
+  margin: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  background-color: #fafafa;
+  &:hover {
+    background-color: #fafafa;
+  }
+`;
+
+export const RemoveButton = styled(Button)`
+  width: 20px;
+  height: 20px;
+  margin: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  background-color: #fafafa;
+
+  align-self: flex-end;
+
+  &:hover {
+    background-color: #fafafa;
+  }
+`;
+
+export const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: auto;
+  background-color: #fafafa;
+  gap: 10px;
+`;
+
+export const ListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: left;
+  justify-content: space-between;
+  width: 300px;
+  gap: 10px;
+`;
+
+export const CustomCheckbox = styled(Checkbox)`
+  width: 20px;
+  height: 20px;
+  margin: 10px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+  padding: 10px;
+  background-color: #fafafa;
+
+  &:hover {
+    background-color: #fafafa;
+  }
+`;
+
+export const CustomText = styled.span`
+  margin-left: 10px;
+`;
