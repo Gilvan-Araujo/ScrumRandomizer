d0038dea0481ca20e41d8649ecff01f59fd35bf9
Gilvan Araújo <gilvanjunior@protonmail.com>
2022-02-09 16:43:47 -0300

    style: main page width and height

--- styles/style.ts
+++ styles/style.ts
@@ -6,8 +6,8 @@ export const Container = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  width: 100vw;
-  height: 100vh;
+  padding: 2rem;
+  margin: 2rem;
   gap: 25px;
 `;
 
